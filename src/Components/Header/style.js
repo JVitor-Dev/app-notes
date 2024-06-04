d3f6e0a45f6cd003cc61550ad4a48e713162acf7
João Vitor <jvitordev@outlook.com>
2024-06-04 16:36:41 -0300

    grid in home

--- src/Components/Header/style.js
+++ src/Components/Header/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.header`
   grid-area: header;
 
-  height: 95px;
+  height: 105px;
   width: 100%;
 
   border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Details} from './pages/Details'
-
 import GlobalStyle from './Styles/global'
+
 import { ThemeProvider } from 'styled-components'
 import theme from './Styles/theme'
+
+import {Home} from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <GlobalStyle/>
-    <Details />
+      <GlobalStyle/>
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
