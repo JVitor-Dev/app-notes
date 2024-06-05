44a6aca534c030351e50545b412e46b8a0d45b0b
João Vitor <jvitordev@outlook.com>
2024-06-05 14:47:09 -0300

    1st part sign in page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyle from './Styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from './Styles/theme'
 
-import {Home} from './pages/Home'
+import {SignIn} from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
