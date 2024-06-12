1dd3acc92e2325bc581aae86a18ed866f9899e1f
João Vitor <jvitordev@outlook.com>
2024-06-12 14:51:20 -0300

    loading user data in profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,8 +3,17 @@ import { Link } from "react-router-dom"
 import {Input} from '../../Components/Input'
 import {Button} from '../../Components/Button'
 import {Container, Form, Avatar} from './style'
+import { useState } from 'react'
+import { useAuth } from "../../hooks/auth"
 
 export function Profile(){
+  const { user } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
   return(
     <Container>
       <header>
@@ -26,21 +35,25 @@ export function Profile(){
         placeholder="Nome"
         type="text"
         icon={FiUser}
+        value={name}
         />
         <Input 
         placeholder="Email"
         type="text"
         icon={FiMail}
+        value={email}
         />
         <Input 
         placeholder="Senha Atual"
         type="password"
         icon={FiLock}
+        onChange={e => setPasswordOld(e.target.value)}
         />
         <Input 
         placeholder="Nova Senha"
         type="password"
         icon={FiLock}
+        onChange={e => setPasswordNew(e.target.value)}
         />
         <Button title="Salvar"/>
       </Form>
