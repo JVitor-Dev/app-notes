7f1115fc01b2fb811cc1dfc537dfc7c920d5f8cc
João Vitor <jvitordev@outlook.com>
2024-06-20 17:35:03 -0300

    fixed bug

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from 'react'
 
-import { api } from '../../../../Stage 08/iniciando-nodejs/src/services/api'
+import { api } from '../../../../Stage 08/rocketnotes-backend/src/services/api'
 
 export const AuthContext = createContext({})
 
